135167ac91e139a907d6361795f5d1d3f94ffb4e
Lakhiwe <siyasangalakhiwe6@gmail.com>
2017-05-19 15:33:43 +0200

    first commit

---
+++ greet_xola.js
@@ -0,0 +1,17 @@
+const chalk = require('chalk');
+//import the greet module that is in the current folder
+const greet = require('./greet');
+
+const styledMessage = chalk.bgBlue.black(greet('Hi Xola'));
+console.log(styledMessage)
+
+var figlet = require('figlet');
+
+figlet('Hi Xola', function(err, data) {
+    if (err) {
+        console.log('Something went wrong...');
+        console.dir(err);
+        return;
+    }
+    console.log(data)
+});
